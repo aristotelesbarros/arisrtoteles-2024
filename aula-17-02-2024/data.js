1d3c0440d6808584bde2d279fbd1e331877cf0a4
Aristoteles Barros Dal Farra <143902784+aristotelesbarros@users.noreply.github.com>
2024-02-17 14:57:06 -0300

    cc-aula-20

---
+++ aula-17-02-2024/data.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        id:"9BlnxoT5x0k",
+        title:"",
+        duration:"",
+        price:""
+    },
+
+    {
+        id:"",
+        title:"",
+        duration:"",
+        price:""
+    },
+
+    {
+        id:"",
+        title:"",
+        duration:"",
+        price:""
+    },
+
+    {
+        id:"",
+        title:"",
+        duration:"",
+        price:""
+    },
+]
---
+++ aula-17-02-2024/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+const videos= require ('./data');
+
+
+server.use(express.static('public'))
+server.set ("view engine","njk")
+nunjucks.configure ("views",{
+    express:server
+})
+server.listen (5000,function(){
+    console.log("hello wolrd!!!")
+    server.get("/", function(req,res){
+        return res.render ("index")
+    })
+
+    server.get("/videos",function (req,res){
+return res.render("videos",{items:videos});
+    })
+})
